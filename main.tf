b5157c1120b15f9c345e3f393b132e42542216bc
rishu-raj007 <rahul747979@gmail.com>
2025-07-11 20:33:23 +0530

    terraform resource gp added

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform{
+    required_providers {
+      azurerm ={
+        source = "hashicorp/azurerm"
+        version = ">=3.0.0"
+      }
+    }
+}
+provider "azurerm" {
+  features {}
+  
+}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
